dd9ed08de69126919c4504e648cf0033bfc0e75d
Nikhil Reddy <129280955+Nikhil1120@users.noreply.github.com>
2025-01-09 18:00:29 +0530

    Update settings.py

--- email_notification_api/settings.py
+++ email_notification_api/settings.py
@@ -129,5 +129,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  # Example using Gmail
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'nikhilt1120@gmail.com'
-EMAIL_HOST_PASSWORD = 'xbnh bcnr iiup opvd'
+EMAIL_HOST_USER = 'abc@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
